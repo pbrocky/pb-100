d82aecf6f266f152345d6c21c68d43dfa496e35c
pbrocky <pbrcky@gmail.com>
2016-02-13 23:55:05 +0900

    add README.md, add sample.html of pbPrinter.css.

--- pbDisplayCSS/0.x/checkActiveX.js
+++ pbDisplayCSS/0.x/checkActiveX.js
@@ -1,4 +1,4 @@
-(function(b, c, e){
+(function(b, c, e, s){
   b = document.body;
   if(!b) return setTimeout(arguments.callee, 50);
   c = b.className || '';
@@ -6,5 +6,6 @@
   b.appendChild(e);
   e.style.cssText = 'position:absolute;top:0;left:0;width:1px;height:1px;line-height:1px;filter:dropshadow() progid:DXImageTransform.Microsoft.Shadow();';
   b.className += ( c ? ' ' : c ) + ( e.offsetHeight > 1 ? 'pbDisplay-ActiveX-enabled' : '' );
+  e.removeAttribute( 'style' ); // before removeChild(e) for ie5
   b.removeChild(e);
 })();
