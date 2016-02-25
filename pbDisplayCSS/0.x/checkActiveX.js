37add607bdc1f409df4a70e672fa1ecc4939655a
pbrocky <pbrcky@gmail.com>
2016-02-25 23:07:29 +0900

    small fixes.

--- pbDisplayCSS/0.x/checkActiveX.js
+++ pbDisplayCSS/0.x/checkActiveX.js
@@ -1,8 +1,9 @@
-(function(b, c, e, s){
-  b = document.body;
+(function(d, b, c, e){
+  d = document;
+  b = d.body;
   if(!b) return setTimeout(arguments.callee, 50);
   c = b.className || '';
-  e = document.createElement('div');
+  e = d.createElement('div');
   b.appendChild(e);
   e.style.cssText = 'position:absolute;top:0;left:0;width:1px;height:1px;line-height:1px;filter:dropshadow() progid:DXImageTransform.Microsoft.Shadow();';
   b.className += ( c ? ' ' : c ) + ( e.offsetHeight > 1 ? 'pbDisplay-ActiveX-enabled' : '' );
